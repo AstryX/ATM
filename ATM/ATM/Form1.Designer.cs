1fcb89e564f2d647c9d65c535c5180732c79929b
RDereskevicius <RDereskevicius@computing.dundee.ac.uk>
2017-03-21 19:36:06 +0000

    First commit. Added some functionality to the buttons

---
+++ ATM/ATM/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace ATM
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.btn3 = new System.Windows.Forms.Button();
+			this.btn5 = new System.Windows.Forms.Button();
+			this.btn7 = new System.Windows.Forms.Button();
+			this.btn2 = new System.Windows.Forms.Button();
+			this.btn4 = new System.Windows.Forms.Button();
+			this.btn6 = new System.Windows.Forms.Button();
+			this.btn1 = new System.Windows.Forms.Button();
+			this.np0 = new System.Windows.Forms.Button();
+			this.acceptBtn = new System.Windows.Forms.Button();
+			this.correctionBtn = new System.Windows.Forms.Button();
+			this.cancelBtn = new System.Windows.Forms.Button();
+			this.np9 = new System.Windows.Forms.Button();
+			this.np6 = new System.Windows.Forms.Button();
+			this.np8 = new System.Windows.Forms.Button();
+			this.np5 = new System.Windows.Forms.Button();
+			this.np7 = new System.Windows.Forms.Button();
+			this.np4 = new System.Windows.Forms.Button();
+			this.np3 = new System.Windows.Forms.Button();
+			this.np2 = new System.Windows.Forms.Button();
+			this.np1 = new System.Windows.Forms.Button();
+			this.btn8 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(301, 12);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(412, 219);
+			this.textBox1.TabIndex = 0;
+			// 
+			// btn3
+			// 
+			this.btn3.Location = new System.Drawing.Point(234, 147);
+			this.btn3.Name = "btn3";
+			this.btn3.Size = new System.Drawing.Size(61, 39);
+			this.btn3.TabIndex = 2;
+			this.btn3.Text = ">";
+			this.btn3.UseVisualStyleBackColor = true;
+			// 
+			// btn5
+			// 
+			this.btn5.Location = new System.Drawing.Point(234, 102);
+			this.btn5.Name = "btn5";
+			this.btn5.Size = new System.Drawing.Size(61, 39);
+			this.btn5.TabIndex = 3;
+			this.btn5.Text = ">";
+			this.btn5.UseVisualStyleBackColor = true;
+			// 
+			// btn7
+			// 
+			this.btn7.Location = new System.Drawing.Point(234, 57);
+			this.btn7.Name = "btn7";
+			this.btn7.Size = new System.Drawing.Size(61, 39);
+			this.btn7.TabIndex = 4;
+			this.btn7.Text = ">";
+			this.btn7.UseVisualStyleBackColor = true;
+			// 
+			// btn2
+			// 
+			this.btn2.Location = new System.Drawing.Point(719, 192);
+			this.btn2.Name = "btn2";
+			this.btn2.Size = new System.Drawing.Size(61, 39);
+			this.btn2.TabIndex = 5;
+			this.btn2.Text = "<";
+			this.btn2.UseVisualStyleBackColor = true;
+			// 
+			// btn4
+			// 
+			this.btn4.Location = new System.Drawing.Point(719, 147);
+			this.btn4.Name = "btn4";
+			this.btn4.Size = new System.Drawing.Size(61, 39);
+			this.btn4.TabIndex = 6;
+			this.btn4.Text = "<";
+			this.btn4.UseVisualStyleBackColor = true;
+			// 
+			// btn6
+			// 
+			this.btn6.Location = new System.Drawing.Point(719, 102);
+			this.btn6.Name = "btn6";
+			this.btn6.Size = new System.Drawing.Size(61, 39);
+			this.btn6.TabIndex = 7;
+			this.btn6.Text = "<";
+			this.btn6.UseVisualStyleBackColor = true;
+			// 
+			// btn1
+			// 
+			this.btn1.Location = new System.Drawing.Point(234, 192);
+			this.btn1.Name = "btn1";
+			this.btn1.Size = new System.Drawing.Size(61, 39);
+			this.btn1.TabIndex = 1;
+			this.btn1.Text = ">";
+			this.btn1.UseVisualStyleBackColor = true;
+			// 
+			// np0
+			// 
+			this.np0.Image = global::ATM.Properties.Resources._0;
+			this.np0.Location = new System.Drawing.Point(79, 509);
+			this.np0.Name = "np0";
+			this.np0.Size = new System.Drawing.Size(61, 39);
+			this.np0.TabIndex = 22;
+			this.np0.UseVisualStyleBackColor = true;
+			// 
+			// acceptBtn
+			// 
+			this.acceptBtn.Image = global::ATM.Properties.Resources.accept;
+			this.acceptBtn.Location = new System.Drawing.Point(213, 464);
+			this.acceptBtn.Name = "acceptBtn";
+			this.acceptBtn.Size = new System.Drawing.Size(107, 39);
+			this.acceptBtn.TabIndex = 21;
+			this.acceptBtn.UseVisualStyleBackColor = true;
+			// 
+			// correctionBtn
+			// 
+			this.correctionBtn.Image = global::ATM.Properties.Resources.correction;
+			this.correctionBtn.Location = new System.Drawing.Point(213, 419);
+			this.correctionBtn.Name = "correctionBtn";
+			this.correctionBtn.Size = new System.Drawing.Size(107, 39);
+			this.correctionBtn.TabIndex = 20;
+			this.correctionBtn.UseVisualStyleBackColor = true;
+			// 
+			// cancelBtn
+			// 
+			this.cancelBtn.Image = global::ATM.Properties.Resources.cancel;
+			this.cancelBtn.Location = new System.Drawing.Point(213, 374);
+			this.cancelBtn.Name = "cancelBtn";
+			this.cancelBtn.Size = new System.Drawing.Size(107, 39);
+			this.cancelBtn.TabIndex = 19;
+			this.cancelBtn.UseVisualStyleBackColor = true;
+			// 
+			// np9
+			// 
+			this.np9.Image = global::ATM.Properties.Resources._9;
+			this.np9.Location = new System.Drawing.Point(146, 464);
+			this.np9.Name = "np9";
+			this.np9.Size = new System.Drawing.Size(61, 39);
+			this.np9.TabIndex = 17;
+			this.np9.UseVisualStyleBackColor = true;
+			// 
+			// np6
+			// 
+			this.np6.Image = global::ATM.Properties.Resources._6;
+			this.np6.Location = new System.Drawing.Point(146, 419);
+			this.np6.Name = "np6";
+			this.np6.Size = new System.Drawing.Size(61, 39);
+			this.np6.TabIndex = 16;
+			this.np6.UseVisualStyleBackColor = true;
+			// 
+			// np8
+			// 
+			this.np8.Image = global::ATM.Properties.Resources._8;
+			this.np8.Location = new System.Drawing.Point(79, 464);
+			this.np8.Name = "np8";
+			this.np8.Size = new System.Drawing.Size(61, 39);
+			this.np8.TabIndex = 15;
+			this.np8.UseVisualStyleBackColor = true;
+			// 
+			// np5
+			// 
+			this.np5.Image = global::ATM.Properties.Resources._5;
+			this.np5.Location = new System.Drawing.Point(79, 419);
+			this.np5.Name = "np5";
+			this.np5.Size = new System.Drawing.Size(61, 39);
+			this.np5.TabIndex = 14;
+			this.np5.UseVisualStyleBackColor = true;
+			// 
+			// np7
+			// 
+			this.np7.Image = global::ATM.Properties.Resources._7;
+			this.np7.Location = new System.Drawing.Point(12, 464);
+			this.np7.Name = "np7";
+			this.np7.Size = new System.Drawing.Size(61, 39);
+			this.np7.TabIndex = 13;
+			this.np7.UseVisualStyleBackColor = true;
+			// 
+			// np4
+			// 
+			this.np4.Image = global::ATM.Properties.Resources._4;
+			this.np4.Location = new System.Drawing.Point(12, 419);
+			this.np4.Name = "np4";
+			this.np4.Size = new System.Drawing.Size(61, 39);
+			this.np4.TabIndex = 12;
+			this.np4.UseVisualStyleBackColor = true;
+			// 
+			// np3
+			// 
+			this.np3.Image = global::ATM.Properties.Resources._3;
+			this.np3.Location = new System.Drawing.Point(146, 374);
+			this.np3.Name = "np3";
+			this.np3.Size = new System.Drawing.Size(61, 39);
+			this.np3.TabIndex = 11;
+			this.np3.UseVisualStyleBackColor = true;
+			// 
+			// np2
+			// 
+			this.np2.Image = global::ATM.Properties.Resources._2;
+			this.np2.Location = new System.Drawing.Point(79, 374);
+			this.np2.Name = "np2";
+			this.np2.Size = new System.Drawing.Size(61, 39);
+			this.np2.TabIndex = 10;
+			this.np2.UseVisualStyleBackColor = true;
+			// 
+			// np1
+			// 
+			this.np1.Image = global::ATM.Properties.Resources._1;
+			this.np1.Location = new System.Drawing.Point(12, 374);
+			this.np1.Name = "np1";
+			this.np1.Size = new System.Drawing.Size(61, 39);
+			this.np1.TabIndex = 9;
+			this.np1.UseVisualStyleBackColor = true;
+			// 
+			// btn8
+			// 
+			this.btn8.Location = new System.Drawing.Point(719, 57);
+			this.btn8.Name = "btn8";
+			this.btn8.Size = new System.Drawing.Size(61, 39);
+			this.btn8.TabIndex = 8;
+			this.btn8.Text = "<";
+			this.btn8.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Gray;
+			this.ClientSize = new System.Drawing.Size(995, 553);
+			this.Controls.Add(this.np0);
+			this.Controls.Add(this.acceptBtn);
+			this.Controls.Add(this.correctionBtn);
+			this.Controls.Add(this.cancelBtn);
+			this.Controls.Add(this.np9);
+			this.Controls.Add(this.np6);
+			this.Controls.Add(this.np8);
+			this.Controls.Add(this.np5);
+			this.Controls.Add(this.np7);
+			this.Controls.Add(this.np4);
+			this.Controls.Add(this.np3);
+			this.Controls.Add(this.np2);
+			this.Controls.Add(this.np1);
+			this.Controls.Add(this.btn8);
+			this.Controls.Add(this.btn6);
+			this.Controls.Add(this.btn4);
+			this.Controls.Add(this.btn2);
+			this.Controls.Add(this.btn7);
+			this.Controls.Add(this.btn5);
+			this.Controls.Add(this.btn3);
+			this.Controls.Add(this.btn1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button btn3;
+		private System.Windows.Forms.Button btn5;
+		private System.Windows.Forms.Button btn7;
+		private System.Windows.Forms.Button btn2;
+		private System.Windows.Forms.Button btn4;
+		private System.Windows.Forms.Button btn6;
+		private System.Windows.Forms.Button btn8;
+		private System.Windows.Forms.Button np1;
+		private System.Windows.Forms.Button np2;
+		private System.Windows.Forms.Button np3;
+		private System.Windows.Forms.Button np4;
+		private System.Windows.Forms.Button np7;
+		private System.Windows.Forms.Button np5;
+		private System.Windows.Forms.Button np8;
+		private System.Windows.Forms.Button np6;
+		private System.Windows.Forms.Button np9;
+		private System.Windows.Forms.Button cancelBtn;
+		private System.Windows.Forms.Button correctionBtn;
+		private System.Windows.Forms.Button acceptBtn;
+		private System.Windows.Forms.Button btn1;
+		private System.Windows.Forms.Button np0;
+	}
+}
+
