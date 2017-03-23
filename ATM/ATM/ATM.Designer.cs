af141e4b57b726c01a79fb5f751c55d5c54fdf43
AndreiDombrovschi <AndreiDombrovschi@computing.dundee.ac.uk>
2017-03-23 15:24:36 +0000

    GUI changes

--- ATM/ATM/ATM.Designer.cs
+++ ATM/ATM/ATM.Designer.cs
@@ -29,12 +29,11 @@
 		private void InitializeComponent()
 		{
             this.btn3 = new System.Windows.Forms.Button();
-            this.btn5 = new System.Windows.Forms.Button();
-            this.btn7 = new System.Windows.Forms.Button();
-            this.btn2 = new System.Windows.Forms.Button();
-            this.btn4 = new System.Windows.Forms.Button();
-            this.btn6 = new System.Windows.Forms.Button();
             this.btn1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.np0 = new System.Windows.Forms.Button();
             this.acceptBtn = new System.Windows.Forms.Button();
             this.correctionBtn = new System.Windows.Forms.Button();
@@ -48,85 +47,72 @@
             this.np3 = new System.Windows.Forms.Button();
             this.np2 = new System.Windows.Forms.Button();
             this.np1 = new System.Windows.Forms.Button();
-            this.btn8 = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn3
             // 
-            this.btn3.Location = new System.Drawing.Point(85, 206);
+            this.btn3.Image = global::ATM.Properties.Resources.rightArrow;
+            this.btn3.Location = new System.Drawing.Point(85, 226);
             this.btn3.Margin = new System.Windows.Forms.Padding(2);
             this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(60, 70);
+            this.btn3.Size = new System.Drawing.Size(60, 60);
             this.btn3.TabIndex = 2;
-            this.btn3.Text = ">";
             this.btn3.UseVisualStyleBackColor = true;
             this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
-            // btn5
-            // 
-            this.btn5.Location = new System.Drawing.Point(85, 132);
-            this.btn5.Margin = new System.Windows.Forms.Padding(2);
-            this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(60, 70);
-            this.btn5.TabIndex = 3;
-            this.btn5.Text = ">";
-            this.btn5.UseVisualStyleBackColor = true;
-            // 
-            // btn7
-            // 
-            this.btn7.Location = new System.Drawing.Point(85, 58);
-            this.btn7.Margin = new System.Windows.Forms.Padding(2);
-            this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(60, 70);
-            this.btn7.TabIndex = 4;
-            this.btn7.Text = ">";
-            this.btn7.UseVisualStyleBackColor = true;
-            // 
-            // btn2
-            // 
-            this.btn2.Location = new System.Drawing.Point(555, 280);
-            this.btn2.Margin = new System.Windows.Forms.Padding(2);
-            this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(60, 70);
-            this.btn2.TabIndex = 5;
-            this.btn2.Text = "<";
-            this.btn2.UseVisualStyleBackColor = true;
-            // 
-            // btn4
-            // 
-            this.btn4.Location = new System.Drawing.Point(555, 206);
-            this.btn4.Margin = new System.Windows.Forms.Padding(2);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(60, 70);
-            this.btn4.TabIndex = 6;
-            this.btn4.Text = "<";
-            this.btn4.UseVisualStyleBackColor = true;
-            // 
-            // btn6
-            // 
-            this.btn6.Location = new System.Drawing.Point(555, 132);
-            this.btn6.Margin = new System.Windows.Forms.Padding(2);
-            this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(60, 70);
-            this.btn6.TabIndex = 7;
-            this.btn6.Text = "<";
-            this.btn6.UseVisualStyleBackColor = true;
-            // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(85, 280);
+            this.btn1.Image = global::ATM.Properties.Resources.rightArrow;
+            this.btn1.Location = new System.Drawing.Point(85, 290);
             this.btn1.Margin = new System.Windows.Forms.Padding(2);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(60, 70);
+            this.btn1.Size = new System.Drawing.Size(60, 60);
             this.btn1.TabIndex = 1;
-            this.btn1.Text = ">";
             this.btn1.UseVisualStyleBackColor = true;
             this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Blue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(150, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 300);
+            this.panel1.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(47, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 32);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Account number:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(53, 99);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 28);
+            this.textBox1.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "label2";
             // 
             // np0
             // 
@@ -271,53 +257,45 @@
             this.np1.UseVisualStyleBackColor = true;
             this.np1.Click += new System.EventHandler(this.np1_Click);
             // 
-            // btn8
+            // btn6
             // 
-            this.btn8.Location = new System.Drawing.Point(555, 58);
-            this.btn8.Margin = new System.Windows.Forms.Padding(2);
-            this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(60, 70);
-            this.btn8.TabIndex = 8;
-            this.btn8.Text = "<";
-            this.btn8.UseVisualStyleBackColor = true;
+            this.btn6.Image = global::ATM.Properties.Resources.leftArrow;
+            this.btn6.Location = new System.Drawing.Point(555, 162);
+            this.btn6.Margin = new System.Windows.Forms.Padding(2);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(60, 60);
+            this.btn6.TabIndex = 7;
+            this.btn6.UseVisualStyleBackColor = true;
             // 
-            // panel1
+            // btn4
             // 
-            this.panel1.BackColor = System.Drawing.Color.Blue;
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(150, 50);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(400, 300);
-            this.panel1.TabIndex = 23;
+            this.btn4.Image = global::ATM.Properties.Resources.leftArrow;
+            this.btn4.Location = new System.Drawing.Point(555, 226);
+            this.btn4.Margin = new System.Windows.Forms.Padding(2);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(60, 60);
+            this.btn4.TabIndex = 6;
+            this.btn4.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // btn2
             // 
-            this.label1.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label1.Location = new System.Drawing.Point(47, 35);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(274, 32);
-            this.label1.TabIndex = 24;
-            this.label1.Text = "Account number:";
+            this.btn2.Image = global::ATM.Properties.Resources.leftArrow;
+            this.btn2.Location = new System.Drawing.Point(555, 290);
+            this.btn2.Margin = new System.Windows.Forms.Padding(2);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(60, 60);
+            this.btn2.TabIndex = 5;
+            this.btn2.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // btn5
             // 
-            this.textBox1.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(53, 99);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(268, 28);
-            this.textBox1.TabIndex = 24;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(69, 412);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 24;
-            this.label2.Text = "label2";
+            this.btn5.Image = global::ATM.Properties.Resources.rightArrow;
+            this.btn5.Location = new System.Drawing.Point(85, 162);
+            this.btn5.Margin = new System.Windows.Forms.Padding(2);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(60, 60);
+            this.btn5.TabIndex = 3;
+            this.btn5.UseVisualStyleBackColor = true;
             // 
             // ATM
             // 
@@ -340,11 +318,9 @@
             this.Controls.Add(this.np3);
             this.Controls.Add(this.np2);
             this.Controls.Add(this.np1);
-            this.Controls.Add(this.btn8);
             this.Controls.Add(this.btn6);
             this.Controls.Add(this.btn4);
             this.Controls.Add(this.btn2);
-            this.Controls.Add(this.btn7);
             this.Controls.Add(this.btn5);
             this.Controls.Add(this.btn3);
             this.Controls.Add(this.btn1);
@@ -362,12 +338,10 @@
 		#endregion
 
         private System.Windows.Forms.Button btn3;
-		private System.Windows.Forms.Button btn5;
-		private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn5;
 		private System.Windows.Forms.Button btn2;
 		private System.Windows.Forms.Button btn4;
-		private System.Windows.Forms.Button btn6;
-		private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn6;
 		private System.Windows.Forms.Button np1;
 		private System.Windows.Forms.Button np2;
 		private System.Windows.Forms.Button np3;
--- ATM/ATM/Properties/Resources.Designer.cs
+++ ATM/ATM/Properties/Resources.Designer.cs
@@ -199,5 +199,25 @@ namespace ATM.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap leftArrow {
+            get {
+                object obj = ResourceManager.GetObject("leftArrow", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rightArrow {
+            get {
+                object obj = ResourceManager.GetObject("rightArrow", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
